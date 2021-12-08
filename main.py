c8f7ff9d56aecc61b6f62b09f63fed33325bd6c8
snailred <kozlovskiyandrey69@gmail.com>
2021-12-09 00:20:04 +0300

    rghrouhgfu

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-
+n = int(input())
+n_triangles = 0
+num = 1
+len = 0
+for i in range(n):
+    l = int(input())
+    if len == l:
+        num+=1
+        if num==3:
+            n_triangles+=1
+            num = 0
+    else:
+        num = 1
+        len = l
+print(n_triangles)
